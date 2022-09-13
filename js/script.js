6636ccb9f196cf0d5eff163d971f7cdfeada1813
obaedulislam <obaed.cse@gmail.com>
2022-09-14 02:56:05 +0600

    Basic Input Validation Done

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Get All Input Field Value by ID
+const getInputValueById = (id) => {
+    const input = document.getElementById(id);
+    const value = input.value;
+    input.value = "";
+    return value;
+}
+
+//Add Product On Item
+const addProduct = () => {
+    const productName = getInputValueById("product-name");
+    const productQuantity = getInputValueById("product-quantity");
+    const number = Number(productQuantity);
+
+    if(!isNaN(productName) || !Number.isInteger(number)){
+        alert("Your Input is not Valid");
+        return;
+    }
+
+}
+
